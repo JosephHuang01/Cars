5211635f53b74a41b7dcaf71476b03ebc7b8de54
JosephHuang01 <kungfujoejoe@gmail.com>
2025-06-03 18:09:54 -0400

    Added tests and altered gas car

--- components/gas_car.py
+++ components/gas_car.py
@@ -1,7 +1,27 @@
-from components.car import Car
+from components.car import Car, Position
 from components.gas_tank import GasTank
     
 class GasCar(Car):
-    def __init__(self, make, model, year, gas_tank_supply):
+    def __init__(self, make, model, year, gas_tank_supply = 15):
         super().__init__(make, model, year)
         self.gas_tank = GasTank(gas_tank_supply)
+        self.gallons_used_per_mile = 0.05
+        miles = 1/self.gallons_used_per_mile
+    
+    def move_with_gas(self, gallons, direction, distance):
+        if gallons > 0:
+            if self.gas_tank.travel_distance(distance, self.gallons_used_per_mile):
+                if direction == "forward":
+                    self.position.y += distance
+                elif direction == "backward":
+                    self.position.y -= distance
+                elif direction == "left":
+                    self.position.x -= distance
+                elif direction == "right":
+                    self.position.x += distance
+        if distance >= 300:
+            GasCar(Car).gas_tank_supply = 0
+            if GasCar(Car).gas_tank_supply == 0:
+                distance += 0
+        if GasCar(Car).read_odometer == 20:
+            GasCar(Car).gas_tank_supply -= 1
--- unit_tests/components/gas_car_unit_test.py
+++ unit_tests/components/gas_car_unit_test.py
@@ -1,5 +1,30 @@
 import unittest
 
-from components.gas_car import GasTank, GasCar
+from components.car import Car, Position
+from components.gas_car import GasCar
+from components.gas_tank import GasTank
 
-car_1 = GasCar('Toyota', 'Corolla', 2017, 15)
+class TestGasCar(unittest.TestCase):
+
+    def test_default_capacity(self):
+        test_gas_tank_1 = GasCar('Toyota', 'Corolla', 2017)
+        test_gas_tank_1.move_with_gas(15, "foward", 3)
+        test_gas_tank_1.drive_forward(1)
+        test_gas_tank_1.turn_right(2)
+        self.assertEqual(test_gas_tank_1.read_odometer(), 3)
+    
+    def test_driving_with_tank_half_full(self):
+        test_gas_tank_3 = GasCar('Hyundai', 'Kona', 2023)
+        test_gas_tank_3.move_with_gas(5, "left", 4)
+        test_gas_tank_3.turn_left(4)
+        self.assertEqual(test_gas_tank_3.read_odometer(), 4)
+
+    def test_refuel(self):
+        test_gas_tank_5 = GasCar('Honda', 'Civic', 2020)
+        test_gas_tank_5.move_with_gas(10, "backward", 10)
+        test_gas_tank_5.drive_backward(6)
+        test_gas_tank_5.turn_left(4)
+        self.assertEqual(test_gas_tank_5.read_odometer(), 10)
+
+if __name__ == '__main__':
+    unittest.main()
